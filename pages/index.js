87b23fd61a1220d429a06d34536095fe85c057db
nacho-accion-futbol <ignaciolopezmora@gmail.com>
2026-06-15 21:45:28 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -119,7 +119,7 @@ export default function Home() {
                 {photos.map((photo, index) => (
                   <div key={index} className={styles.photoItem}>
                     <img
-                      src={selectedPhoto.thumbnailLink || selectedPhoto.webContentLink}
+                      src={selectedPhoto?.webContentLink}
                       alt={photo.name}
                       className={styles.photoImg}
                       loading="lazy"
@@ -139,7 +139,7 @@ export default function Home() {
           <div className={styles.photoModalContent} onClick={(e) => e.stopPropagation()}>
             <button className={styles.closeBtn} onClick={closePhotoModal}>✕</button>
             <img
-             src={selectedPhoto.thumbnailLink || selectedPhoto.webContentLink}
+             src={selectedPhoto?.webContentLink}
               alt={selectedPhoto.name}
               className={styles.photoModalImg}
             />
